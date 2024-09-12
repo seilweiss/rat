ad6721de8b9c4da3eaa57a12df8be7c2b4d4e127
Seil Weiss <32021834+seilweiss@users.noreply.github.com>
2024-09-11 23:24:07 -0500

    Match p2dep.c

--- configure.py
+++ configure.py
@@ -1420,7 +1420,7 @@ config.libs = [
             Object(Matching, "rwsdk/src/pipe/p2/p2altmdl.c"),
             Object(Matching, "rwsdk/src/pipe/p2/p2core.c"),
             Object(Matching, "rwsdk/src/pipe/p2/p2define.c"),
-            Object(NonMatching, "rwsdk/src/pipe/p2/p2dep.c"),
+            Object(Matching, "rwsdk/src/pipe/p2/p2dep.c"),
             Object(NonMatching, "rwsdk/src/pipe/p2/p2heap.c"),
             Object(NonMatching, "rwsdk/src/pipe/p2/p2renderstate.c"),
             Object(NonMatching, "rwsdk/src/pipe/p2/p2resort.c"),
